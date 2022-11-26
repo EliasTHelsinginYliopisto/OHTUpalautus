d3efbd70c66754c634a5781157248a7bac9ba010
Elias Toukolehto <elias.toukolehto@helsinki.fi>
2022-11-26 19:39:57 +0200

    vk4 t5.2

--- viikko4/tdd-ostoskori/src/index.py
+++ viikko4/tdd-ostoskori/src/index.py
@@ -1,1 +1,13 @@
 # testikoodi tänne jos tarvetta
+from ostos import Ostos
+from ostoskori import Ostoskori
+from tuote import Tuote
+
+def main():
+    kori = Ostoskori()
+    maito = Tuote("Maito", 3)
+    kori.lisaa_tuote(maito)
+    print(kori.ostokset)
+
+if __name__ == "__main__":
+    main()
